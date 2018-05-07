12a1b96b94dcd45590f585cf0676fecbca71610a
laishaojiang <308117136@qq.com>
2018-05-07 16:00:34 +0800

    新增路由和页面

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="login">
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Login',
+  data () {
+      return {
+          msg:'This is login page'
+      }
+  }
+}
+</script>
+
+<style>
+    h1{
+        color: red;
+    }
+</style>
